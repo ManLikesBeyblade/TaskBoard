eefba0cc92498b58d916421ce703bc438d050ec3
laaron247 <25886263@stu.mmu.ac.uk>
2026-01-22 14:58:16 +0000

    idk

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,34 +1,36 @@
 import javax.swing.*;
 import java.awt.event.*;
-public class MyPanel {
 
-    public class MyPanel extends JPanel {
-        JLabel myLabel; //a text label
-        JTextField myTextField;
-        JButton myButton;
-        ActionEvent event;
+public class MyPanel extends JPanel {
 
-        //constructor
-        MyPanel(String text) {
-            myLabel = new JLabel(text);
-            add(myLabel); //place component on panel
 
-            //create a text field 20 characters wide
-            myTextField = new JTextField(20);
-            add(myTextField); //place component on panel
+    JLabel myLabel; //a text label
+    JTextField myTextField;
+    JButton myButton;
+    ActionEvent event;
 
-            myButton = new JButton("Greet");
-            myButton.addActionListener(this); //associate an event with button
-            add(myButton);
+    //constructor
+    MyPanel(String text) {
+        myLabel = new JLabel(text);
+        add(myLabel); //place component on panel
 
-            //methods: implement interface method for actionListener
-            void actionPerformed(ActionEvent event){
-                if (event.getSource() == myButton) {
-                    System.out.println("d");
-                }
-            }
+        //create a text field 20 characters wide
+        myTextField = new JTextField(20);
+        add(myTextField); //place component on panel
+
+        myButton = new JButton("Greet");
+        myButton.addActionListener((ActionListener) this); //associate an event with button
+        add(myButton);
+
+
+    }
+
+    //methods: implement interface method for actionListener
+    void actionPerformed(ActionEvent event) {
+        if (event.getSource() == myButton) {
+            System.out.println("d");
         }
+    }
 
 
-    }//endclass
-}
+}//endclass
