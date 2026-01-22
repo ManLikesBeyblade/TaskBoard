dcb89d9036782b3000831d39e4b7ce1e65085cb1
25889645 <zatchimartin@gmail.com>
2026-01-22 12:06:48 +0000

    hello world

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util; //import string
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Hello and world :D!");
+        //hello
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+    }
+}
+
+
