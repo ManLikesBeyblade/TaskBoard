5eb91281a8fb7d7537b964e3815208225c3f91a8
ali <alihkennedy06@gmail.com>
2026-01-23 11:06:49 +0000

    displays screen

--- src/AppGUI/AppLayout.java
+++ src/AppGUI/AppLayout.java
@@ -1,16 +1,23 @@
 package AppGUI;
+
 import javax.swing.*;
 
 public class AppLayout {
     private JPanel rootPanel = new JPanel();
-    private JScrollBar scrollBar1;
-    private JTextField helloTextField;
+    private JTabbedPane tabbedPane1;
 
-    static void main(String[] args) {
+    public AppLayout() {
+        PrepareApp();
+    }
+    public static void main(String[] args) {
+        AppLayout layout = new AppLayout();
+    }
+
+    private void PrepareApp() {
         JFrame frame = new JFrame("My GUI App");
-        frame.setContentPane(new AppLayout().rootPanel);
+        frame.setSize(1000,1000);
+        frame.setContentPane(this.rootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
         frame.setVisible(true);
     }
 }
